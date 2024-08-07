fdb8d87e7de96f86afb2ae3c517996a37f8b8f58
jarvma18 <markusmjarvinen@outlook.com>
2024-08-07 23:17:32 +0300

    Testing Qt UI and such..

--- app.py
+++ app.py
@@ -1,13 +1,52 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QAction, QFileDialog, QMessageBox
 
 class MyApp(QMainWindow):
-  def __init__(self):
-    super().__init__()
-    self.setWindowTitle('Hello PyQt')
-    self.setGeometry(100, 100, 200, 80)
-    label = QLabel('Hello, world!', self)
-    self.setCentralWidget(label)
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle('Password Manager')
+        self.setGeometry(100, 100, 400, 200)
+        self.initUI()
+
+    def initUI(self):
+        # Create a label
+        self.label = QLabel('Welcome to Password Manager!', self)
+        self.setCentralWidget(self.label)
+
+        # Create a menu bar
+        menubar = self.menuBar()
+
+        # Add file menu
+        fileMenu = menubar.addMenu('File')
+
+        # Add actions to the file menu
+        newDbAction = QAction('New Database', self)
+        newDbAction.triggered.connect(self.create_new_database)
+        fileMenu.addAction(newDbAction)
+
+        openDbAction = QAction('Open Database', self)
+        openDbAction.triggered.connect(self.open_existing_database)
+        fileMenu.addAction(openDbAction)
+
+        quitAction = QAction('Quit', self)
+        quitAction.triggered.connect(self.quit_application)
+        fileMenu.addAction(quitAction)
+
+    def create_new_database(self):
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getSaveFileName(self, "Create New Database", "", "SQLite Files (*.db);;All Files (*)", options=options)
+        if fileName:
+            QMessageBox.information(self, 'Success', 'New database created successfully!')
+
+    def open_existing_database(self):
+        options = QFileDialog.Options()
+        fileName, _ = QFileDialog.getOpenFileName(self, "Open Existing Database", "", "SQLite Files (*.db);;All Files (*)", options=options)
+        if fileName:
+            self.label.setText(f'Opened database: {fileName}')
+            # Here you can add more functionality to interact with the opened database
+
+    def quit_application(self):
+        QApplication.quit()
 
 if __name__ == '__main__':
   app = QApplication(sys.argv)
