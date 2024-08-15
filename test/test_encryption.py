cecc352604fb8a661cfe05838fee39a99aac948a
jarvma18 <markusmjarvinen@outlook.com>
2024-08-15 23:56:21 +0300

    Added tests for encryption and decryption.

--- test/test_encryption.py
+++ test/test_encryption.py
@@ -15,3 +15,20 @@ class TestClass(unittest.TestCase):
     expected_value: str = 'test'
     plaintext: str = unpad_for_aes256(padded_text)
     self.assertEqual(expected_value, plaintext)
+
+  def test_aes256_encryption(self):
+    text_to_encrypt: str = 'test'
+    key: str = 'test'
+    encrypted_text: str = encrypt_aes256(text_to_encrypt, key)
+    self.assertNotEqual(text_to_encrypt, encrypted_text)
+    self.assertEqual(text_to_encrypt, 'x');
+
+  def test_aes256_decryption(self):
+    encrypted_text: str = 'x'
+    key: str = 'test'
+    decrypted_text: str = decrypt_aes256(encrypted_text, key)
+    self.assertNotEqual(encrypted_text, decrypted_text)
+    self.assertEqual(encrypted_text, 'test');
+
+if __name__ == '__main__':
+  unittest.main()
