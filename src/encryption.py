8e58821b998d6b96ad9be23db7e0702ba2f4f144
jarvma18 <markusmjarvinen@outlook.com>
2024-08-16 00:52:22 +0300

    Added encrypt and decrypt, not working correctly yet.

--- src/encryption.py
+++ src/encryption.py
@@ -12,3 +12,17 @@ def pad_for_aes256(plaintext: str) -> str:
 
 def unpad_for_aes256(plaintext: str) -> str:
   return plaintext.rstrip()
+
+def encrypt_aes256(text: str, key: str) -> str:
+  initialization_vector: bytes = Random.new().read(AES.block_size)
+  cipher = AES.new(key, AES.MODE_CBC, initialization_vector)
+  padded_text: str = pad_for_aes256(text)
+  encrypted_text: str = cipher.encrypt(padded_text)
+  return base64.b64encode(initialization_vector + encrypted_text).decode()
+
+def decrypt_aes256(encrypted_text: str, key: str) -> str:
+  encrypted_text: str = base64.b64decode(encrypted_text)
+  initialization_vector: bytes = encrypted_text[:AES.block_size]
+  cipher = AES.new(key, AES.MODE_CBC, initialization_vector)
+  decrypted_text: str = cipher.decrypt(encrypted_text[AES.block_size:])
+  return unpad_for_aes256(decrypted_text.decode())
