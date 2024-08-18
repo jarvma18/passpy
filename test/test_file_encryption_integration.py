9b8e5bac4220f2265de78e67e0dedd926cc2658a
jarvma18 <markusmjarvinen@outlook.com>
2024-08-19 00:13:35 +0300

    Added test for file and encryption integration.

---
+++ test/test_file_encryption_integration.py
@@ -0,0 +1,24 @@
+import unittest
+import os
+
+from src.file import read_file
+from src.file import write_file
+from src.encryption import AES256Cipher
+
+class TestClass(unittest.TestCase):
+  test_file: str = 'test/test_file_encrypted.json'
+  test_file_content: str = '{"test": "test"}'
+  aes_256_cipher: str = AES256Cipher('test')
+
+  def delete_file(self) -> None:
+    if os.path.exists(self.test_file):
+      os.remove(self.test_file)
+
+  def test_should_create_file_and_encrypt_and_decrypt_content(self):
+    encrypted_content: str = AES256Cipher.encrypt(self.aes_256_cipher, self.test_file_content)
+    write_file(self.test_file, encrypted_content)
+    encrypted_file_content: str = read_file(self.test_file)
+    self.assertNotEqual(self.test_file_content, encrypted_file_content)
+    decrypted_file_content: str = AES256Cipher.decrypt(self.aes_256_cipher, encrypted_file_content)
+    self.assertEqual(self.test_file_content, decrypted_file_content)
+    self.delete_file()
