59db13bf2a44d00db8c40c11c8d73bf7a8c2c693
jarvma18 <markusmjarvinen@outlook.com>
2024-08-18 23:45:45 +0300

    Fixed all the functions a bit after research online, changed aes block size to 16.

--- src/encryption.py
+++ src/encryption.py
@@ -7,22 +7,25 @@ import os
 def pad_for_aes256(plaintext: str) -> str:
   block_size = 16
   remainder = len(plaintext) % block_size
-  padding_needed = block_size - remainder
-  return plaintext + padding_needed * ' '
+  padding = block_size - remainder
+  return plaintext + chr(padding) * padding
 
-def unpad_for_aes256(plaintext: str) -> str:
-  return plaintext.rstrip()
+def unpad_for_aes256(plaintext: bytes) -> str:
+  padding = plaintext[-1]
+  return plaintext[:-padding].decode('utf-8')
 
 def encrypt_aes256(text: str, key: str) -> str:
-  initialization_vector: bytes = Random.new().read(AES.block_size)
-  cipher = AES.new(key, AES.MODE_CBC, initialization_vector)
-  padded_text: str = pad_for_aes256(text)
-  encrypted_text: str = cipher.encrypt(padded_text)
+  private_key: str = hashlib.sha256(key.encode("utf-8")).digest()
+  raw_text: bytes = str.encode(pad_for_aes256(text))
+  initialization_vector: bytes = Random.new().read(16)
+  cipher = AES.new(private_key, AES.MODE_CBC, initialization_vector)
+  encrypted_text: str = cipher.encrypt(raw_text)
   return base64.b64encode(initialization_vector + encrypted_text).decode()
 
 def decrypt_aes256(encrypted_text: str, key: str) -> str:
+  private_key = hashlib.sha256(key.encode("utf-8")).digest()
   encrypted_text: str = base64.b64decode(encrypted_text)
-  initialization_vector: bytes = encrypted_text[:AES.block_size]
-  cipher = AES.new(key, AES.MODE_CBC, initialization_vector)
-  decrypted_text: str = cipher.decrypt(encrypted_text[AES.block_size:])
-  return unpad_for_aes256(decrypted_text.decode())
+  initialization_vector: bytes = encrypted_text[:16]
+  cipher = AES.new(private_key, AES.MODE_CBC, initialization_vector)
+  decrypted_text: str = cipher.decrypt(encrypted_text[16:])
+  return unpad_for_aes256(decrypted_text)
