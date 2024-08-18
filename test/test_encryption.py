caff02b631b4b6c9594bbad71f90c31190a56e81
jarvma18 <markusmjarvinen@outlook.com>
2024-08-18 23:46:02 +0300

    Fixed tests and now those are working.

--- test/test_encryption.py
+++ test/test_encryption.py
@@ -8,12 +8,12 @@ from src.encryption import decrypt_aes256
 class TestClass(unittest.TestCase):
   def test_should_pad_text_for_aes256(self):
     plaintext: str = 'test'
-    expected_value: str = 'test            '
+    expected_value: str = 'test\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c'
     padded_text: str = pad_for_aes256(plaintext)
     self.assertEqual(expected_value, padded_text)
 
   def test_should_unpad_text(self):
-    padded_text: str = 'test            '
+    padded_text = 'test\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c\x0c'.encode('utf-8')
     expected_value: str = 'test'
     plaintext: str = unpad_for_aes256(padded_text)
     self.assertEqual(expected_value, plaintext)
@@ -23,14 +23,13 @@ class TestClass(unittest.TestCase):
     key: str = 'test'
     encrypted_text: str = encrypt_aes256(text_to_encrypt, key)
     self.assertNotEqual(text_to_encrypt, encrypted_text)
-    self.assertEqual(text_to_encrypt, 'x');
 
   def test_should_decrypt_text_to_aes256(self):
-    encrypted_text: str = 'x'
+    text_to_encrypt: str = 'test'
     key: str = 'test'
+    encrypted_text: str = encrypt_aes256(text_to_encrypt, key)
     decrypted_text: str = decrypt_aes256(encrypted_text, key)
     self.assertNotEqual(encrypted_text, decrypted_text)
-    self.assertEqual(encrypted_text, 'test');
 
 if __name__ == '__main__':
   unittest.main()
