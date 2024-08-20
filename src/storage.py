95afcfa1c208330574545cd76c3324cce5a26560
jarvma18 <markusmjarvinen@outlook.com>
2024-08-21 00:14:05 +0300

    Added encryption and decryption methods to Storage class when reading or writing data.

--- src/storage.py
+++ src/storage.py
@@ -1,8 +1,16 @@
-from file import read_file, write_file
+from src.file import read_file, write_file
+from src.encryption import AES256Cipher
 
 class Storage:
-  def __init__(self, path: str):
+  def __init__(self, path: str, key: str):
     self.path = path
+    self.cipher = AES256Cipher(key)
 
   def read(self) -> str:
-    return read_file(self.path)
+    encrypted_content = read_file(self.path)
+    decrypted_content = self.cipher.decrypt(encrypted_content)
+    return decrypted_content
+
+  def write(self, content: str) -> None:
+    encrypted_content = self.cipher.encrypt(content)
+    write_file(self.path, encrypted_content)
